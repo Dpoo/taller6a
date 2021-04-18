f2b906b311639617ad8200aa27a81141cc69fa31
JulianaG1524 <69610870+JulianaG1524@users.noreply.github.com>
2021-04-18 18:41:46 -0500

    Update PanelMarcador.java

--- src/interfaz/PanelMarcador.java
+++ src/interfaz/PanelMarcador.java
@@ -1,20 +1,34 @@
 package interfaz;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 public class PanelMarcador extends JPanel{
 	
 	private JLabel jugadas;
 	private VentanaJuego juego;
+	private int conteo;
 
 	public PanelMarcador(VentanaJuego vjuego) {
 		
 		juego=vjuego;
-		jugadas = new JLabel("0");
-		add(jugadas, BorderLayout.CENTER);
+		conteo= juego.getJugadas();
+		jugadas = new JLabel(Integer.toString(conteo));
+		setLayout (new BorderLayout());
+		add(jugadas,BorderLayout.CENTER);
+		
+	}
+
+	public void setJugadas(JLabel jugadas) {
+		removeAll();
+		this.jugadas = jugadas;
+		setLayout (new BorderLayout());
+		add(jugadas,BorderLayout.CENTER);
 	}
 
 }
