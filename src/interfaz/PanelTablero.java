895fc5789d21891ef0e345059e47f8600c94f83e
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2021-04-18 16:23:11 -0500

    Colores

--- src/interfaz/PanelTablero.java
+++ src/interfaz/PanelTablero.java
@@ -1,7 +1,56 @@
 package interfaz;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class PanelTablero extends JPanel{
 
+public class PanelTablero extends JPanel implements ActionListener {
+    private int largo;
+    private ArrayList<ArrayList< JButton>> botones;
+    public PanelTablero(int largo){
+        this.largo = largo;
+        this.botones =  new ArrayList<>();
+        GridLayout gl = new GridLayout(largo,largo);
+        setLayout(gl);
+
+        for (int i = 0; i < this.largo; i++) {
+            ArrayList<JButton> acF = new ArrayList<>();
+            botones.add(acF);
+            for (int j = 0; j < this.largo; j++) {
+                JButton btn = new JButton("Apagado");
+                btn.addActionListener(this);
+                btn.setActionCommand(i +","+ j);
+                add(btn);
+                acF.add(btn);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String[] grito = e.getActionCommand().split(",");
+        int i = Integer.parseInt(grito[0]);
+        int j = Integer.parseInt(grito[1]);
+        System.out.println(i+","+j);
+        for (int dx = -1; dx < 2; dx++) {
+            if(dx + i >= 0 &  dx + i < largo){
+                for (int dy = -1; dy < 2; dy++) {
+                    if(dy + j >= 0 &  dy + j < largo & dy*dy +dx*dx <=1){
+                        JButton acb = botones.get(i+dx).get(j+dy);
+                        if(acb.getText().equals("Apagado")){
+                            acb.setText("Encendido");
+                        }else{
+                            acb.setText("Apagado");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
 }
--- src/interfaz/VentanaJuego.java
+++ src/interfaz/VentanaJuego.java
@@ -13,16 +13,26 @@ public class VentanaJuego extends JFrame{
 	 private PanelMarcador panelMarcador; 
 	 private PanelTablero panelTablero;
 	 private PanelBotones panelBotones;
+	 
 	 private Tablero tablero = new Tablero(5);
+	 private Tablero tableroOriginal;
+	 private int tamanio = 5;
+	 private int dificultad = 0;
 	
 	
 		public void establecerDificultad(int i) {
-			tablero.desordenar(i);
+			System.out.print("actual: " + this.dificultad + "\n");
+			this.dificultad = i;
+			System.out.print("nuevo: " + this.dificultad + "\n");
 		}	
 		
 		
 		public void establecerTamanio(int i) {
-			tablero = new Tablero(i);
+			System.out.print("actual: " + this.tamanio + "\n");
+			this.tamanio = i;
+			tablero = new Tablero(tamanio);
+//			panelTablero = new PanelTablero(tablero);
+			System.out.print("nuevo: " + this.tamanio + "\n");
 		}
 
 
@@ -31,18 +41,22 @@ public class VentanaJuego extends JFrame{
 		setSize(700, 700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panelOpciones = new PanelOpciones(this); 
-		panelMarcador = new PanelMarcador(); 
-		panelTablero = new PanelTablero();
+		panelMarcador = new PanelMarcador();
+		panelTablero = new PanelTablero(tamanio);
 		panelBotones = new PanelBotones();
 		  
 		 setLayout(new BorderLayout());
 		 
 		 this.add(panelOpciones , BorderLayout.NORTH); 
 		 this.add(panelMarcador,BorderLayout.WEST);
-		 this.add(panelTablero,BorderLayout.EAST);
+		 this.add(panelTablero,BorderLayout.CENTER);
 		 this.add(panelBotones, BorderLayout.SOUTH);
-		 
-		
+
+		int panelX = (getWidth() - panelTablero.getWidth() - getInsets().left - getInsets().right) / 2;
+		int panelY = ((getHeight() - panelTablero.getHeight() - getInsets().top - getInsets().bottom) / 2);
+
+		panelTablero.setLocation(panelX, panelY);
+
 		setVisible(true);
 	}
 	
